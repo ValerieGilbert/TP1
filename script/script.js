a23820f8a62cb96c50a6f3fb92fe132a13056ea0
Valerie Gilbert <markikiki@hotmail.com>
2017-02-23 13:25:54 -0500

    ajout dun commentaire dans le script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,3 @@
 // Les event listener marchent juste pas sur ma page pour une raison que j'ignore, j'ai donc abandonné les animations en js =(
+// animer du SVG en css est très facile et pratique, mais en javascript, il y a très peu de référence
+// et les exemples fournis nécessitent tous l'intégration de plugins jQuery.
